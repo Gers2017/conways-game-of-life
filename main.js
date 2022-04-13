eb9043bebf31d7725def5ef430c3584e2688dd15
Gers2017 <Gers2017@outlook.com>
2022-04-13 07:32:39 -0600

    use requestAnimationFrame add click event on cell reset board canvas width and height

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // @ts-check
 
-import './style.css'
+import "./style.css";
 
 class Cell {
+  isAlive = false;
   constructor(x, y, w, h) {
     this.rect = createRect(x, y, w, h);
+    this.rect.addEventListener("click", (e) => {
+      this.setAlive(!this.isAlive);
+    });
     this.setAlive(false);
   }
   setAlive(isAlive) {
@@ -31,39 +35,51 @@ function createRect(x, y, w, h) {
   rect.setAttributeNS(null, "stroke-width", "0.1px");
   return rect;
 }
+const canvas_width = 960;
+const canvas_height = 640;
 
-const screen_size = 800;
-const cells_count = 80;
-const cell_size = Math.round(screen_size / cells_count);
-const interval_ms = 200;
-let cell_chance = 0.5;
+const cell_size = 8;
+const rows_count = Math.floor(canvas_width / cell_size);
+const cols_count = Math.floor(canvas_height / cell_size);
+let cell_chance = 0.33;
 
-const svg = document.querySelector('#mainsvg');
-svg.setAttributeNS(null, "width", screen_size.toString());
-svg.setAttributeNS(null, "height", screen_size.toString());
+const svg = document.querySelector("#mainsvg");
+svg.setAttributeNS(null, "width", canvas_width.toString());
+svg.setAttributeNS(null, "height", canvas_height.toString());
 
 /** @type { Cell[][] } */
-const cells = new Array(cells_count).fill(0);
+const cells = new Array(rows_count).fill(0);
 for (let i = 0; i < cells.length; i++) {
-  cells[i] = new Array(cells_count).fill(0);
+  cells[i] = new Array(cols_count).fill(0);
 }
 
 function setup_cells() {
-  svg.childNodes.forEach( node => {
-    svg.removeChild(node);
-  })
-  for (let x = 0; x < cells_count; x++) {
-    for (let y = 0; y < cells_count; y++) {
+  for (let x = 0; x < rows_count; x++) {
+    for (let y = 0; y < cols_count; y++) {
       const posx = cell_size * x;
       const posy = cell_size * y;
       const cell = new Cell(posx, posy, cell_size, cell_size);
       svg.appendChild(cell.rect);
-  
+
       if (Math.random() <= cell_chance) {
         cell.setAlive(true);
       }
-  
+
       cells[x][y] = cell;
+    }
+  }
+}
+
+function restart() {
+  for (let x = 0; x < rows_count; x++) {
+    for (let y = 0; y < cols_count; y++) {
+      const cell = cells[x][y];
+
+      cell.setAlive(false);
+
+      if (Math.random() <= cell_chance) {
+        cell.setAlive(true);
+      }
     }
   }
 }
@@ -77,29 +93,30 @@ const directions = [
   [1, -1],
   [-1, 1],
   [-1, -1],
-]
+];
 
 function simulate() {
-  for (let x = 0; x < cells_count; x++) {
-    for (let y = 0; y < cells_count; y++) {
+  for (let x = 0; x < rows_count; x++) {
+    for (let y = 0; y < cols_count; y++) {
       let n = 0;
-  
-      directions.forEach(([dx, dy])=> {
+
+      directions.forEach(([dx, dy]) => {
         let nx = x + dx;
         let ny = y + dy;
-        if (nx >= cells_count || nx < 0
-          || ny >= cells_count || ny < 0) return;
-  
+        if (nx >= rows_count || nx < 0 || ny >= cols_count || ny < 0) return;
+
         if (cells[nx][ny].isAlive) {
-          n += 1
+          n += 1;
         }
-      })
-  
-      if (n <= 1 || n >= 4) { // cell dies by solitude or cell dies by overpopulation
+      });
+
+      if (n < 2 || n > 3) {
+        // cell dies by underpopulation or cell dies by overpopulation
         cells[x][y].setAlive(false);
       }
-  
-      if (n == 3) { // Each cell with three neighbors becomes populated
+
+      if (n == 3) {
+        // Each cell with exactly three living neighbors comes to life
         cells[x][y].setAlive(true);
       }
     }
@@ -124,39 +141,41 @@ function setChanceLabel(value) {
 setChanceLabel(cell_chance);
 
 const changec_input = CreateRange("#cell_chance", cell_chance);
-changec_input.onchange = (e)=> {
+changec_input.oninput = (e) => {
   const value = Number(e.target.value);
   cell_chance = value;
   setChanceLabel(value);
-}
+};
 
-function createInterval() {
-  return setInterval(()=>{
-    simulate();
-  }, interval_ms);
+let is_active = false;
+
+function play(t) {
+  simulate();
+  if (is_active) {
+    window.requestAnimationFrame(play);
+  }
 }
 
 setup_cells();
-/** @type { NodeJS.Timer } */
-let interval = createInterval();
 
 /** @type { HTMLButtonElement } */
 const start_btn = document.querySelector("#start");
-start_btn.onclick = ()=> {
-  setup_cells();
-  interval = createInterval();
-}
+start_btn.onclick = () => {
+  is_active = true;
+  window.requestAnimationFrame(play);
+};
 
 /** @type { HTMLButtonElement } */
-const reset_btn = document.querySelector("#reset");
-reset_btn.onclick = ()=> {
-  clearInterval(interval);
-  setup_cells();
-  interval = createInterval();
-}
+const restart_btn = document.querySelector("#restart");
+restart_btn.onclick = () => {
+  is_active = false;
+  restart();
+  is_active = true;
+  window.requestAnimationFrame(play);
+};
 
 /** @type { HTMLButtonElement } */
 const stop_btn = document.querySelector("#stop");
 stop_btn.onclick = () => {
-  clearInterval(interval);
+  is_active = false;
 };
